6e5a79191741efbc7c9fb119557bda8a698a3856
balidinesh18 <67181766+balidinesh08@users.noreply.github.com>
2022-02-28 20:11:23 +0530

    v-2

--- oComponents/authComponents/BeginHome.js
+++ oComponents/authComponents/BeginHome.js
@@ -13,7 +13,7 @@ export default function VoteTelugu(props) {
         {/* <ImageBackground 
         style={{resizeMode: 'cover',width: '100%', height: '80%',zIndex:100}}
         source={{uri:"https://i.imgur.com/Rwk9FrE.png"}} /> */}
-        <Image source={{uri:'https://i.imgur.com/Rwk9FrE.png'}} style={{ resizeMode: 'cover', width: '100%', height: '90%',margin:0,padding:0}}/>
+        <Image source={{uri:'https://i.imgur.com/Rwk9FrE.png'}} style={{ resizeMode: 'cover', width: '100%', height: '80%',margin:0,padding:0}}/>
             <Button title="SignUp" style={{height:'5%',width:'2%'}} onPress={()=>{Actions.signup()}}/>
             <Button title="SignIn" onPress={()=>{Actions.signin()}}/>
       </SafeAreaView>
